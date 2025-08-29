6b6e56cb5b0bc20fa033c08a0f0946a140eec803
Xingyu <taybson@gmail.com>
2025-08-28 20:02:19 -0700

    Update route to use neon.

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,10 +1,11 @@
 
-'use server'
 
-import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
+import { neon } from '@neondatabase/serverless';
 
 export async function GET() {
+ const sql = neon(`${process.env.DATABASE_URL}`);
+  
   try {
     const posts = await sql`SELECT * FROM posts ORDER BY date DESC LIMIT 2;`;
     return NextResponse.json({ posts }, { status: 200 });
@@ -19,6 +20,7 @@ export async function POST(request: Request) {
   const title = searchParams.get('title');
   const content = searchParams.get('content');
   const date = searchParams.get('date');
+  const sql = neon(`${process.env.DATABASE_URL}`);
 
   try {
     await sql`INSERT INTO POSTS (id, author, title, content, date) VALUES (${id}, 'Steven', ${title}, ${content}, ${date})`;
