e77af5c754bb06d47b2be3c42468b3a6dea40822
Xingyu <taybson@gmail.com>
2025-08-28 19:27:22 -0700

    Fixing type errors.

--- src/app/blog/post/insert/page.tsx
+++ src/app/blog/post/insert/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
     date: new Date().toISOString().slice(0, 10)
   });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement|HTMLTextAreaElement>) => {
     const { name, value } = e.target;
     setFormData(prevData => ({
       ...prevData,
@@ -20,7 +20,7 @@ export default function Page() {
     }))
   };
 
-  const handleSubmit = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const uuid = uuidv4();
     fetch(`/api/posts?id=${uuid}&title=${formData.title}&content=${formData.content}&date=${formData.date}`, {
@@ -52,7 +52,7 @@ export default function Page() {
         </div>
         <div>
           <label htmlFor="content" className="block font-medium">Content:</label>
-          <textarea id="content" name="content" rows="4" value={formData.content} onChange={handleChange} className="w-full border-2 border-purple-100 p-2 rounded-md focus:border-purple-200 focus:outline-none"></textarea>
+          <textarea id="content" name="content" rows={4} value={formData.content} onChange={handleChange} className="w-full border-2 border-purple-100 p-2 rounded-md focus:border-purple-200 focus:outline-none"></textarea>
         </div>
         <div>
           <label htmlFor="date" className="block font-medium">Date:</label>
